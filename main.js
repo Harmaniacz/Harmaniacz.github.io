fb22f113092234a5c1e0b038a3c1a5d834fd728f
Riki <rmcal024@uottawa.ca>
2024-10-18 20:50:28 -0400

    Added tentative download page, added home redirect to index.html, developed framework for contact.html

--- main.js
+++ main.js
@@ -2,3 +2,7 @@ function myFunction() {
     window.open('https://github.com/Harmaniacz','_blank');
     console.log("The Harmaniacz were here!")
 }
+
+function myFunction_2() {
+    window.open('https://github.com/Harmaniacz/home.html','_blank');
+}
