05fab3b15db7fad23f04fafc064c36e9a152e535
AidanLam06 <aidanlam02@gmail.com>
2024-10-20 16:46:39 -0400

    download page + ui changes & variable name changes
    - created downloads page rough
    - harmony logo in top left now redirects to  index.html
    - variables in main.js changed to be more readble and repeatable; clarified

--- main.js
+++ main.js
@@ -1,8 +1,28 @@
-function myFunction() {
+function github() {
     window.open('https://github.com/Harmaniacz','_blank');
     console.log("The Harmaniacz were here!")
 }
 
-function myFunction_2() {
+function home() {
     window.open('home.html','_blank');
+}
+
+function index() {
+    window.open('index.html', '_blank');
+}
+
+function features() {
+    window.open('features.html', '_blank');
+}
+
+function pricing() {
+    window.open('pricing.html', '_blank');
+}
+
+function download() {
+    window.open('download.html', '_blank');
+}
+
+function team() {
+    window.open('team.html', '_blank');
 }
