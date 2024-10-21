7d9aceba9d025d2e200a7ad81af9c2751af96b8f
Riki <rmcal024@uottawa.ca>
2024-10-21 14:08:05 -0400

    Removed target=_blank in navigation links

--- main.js
+++ main.js
@@ -1,28 +1,28 @@
 function github() {
-    window.open('https://github.com/Harmaniacz','_blank');
+    window.open('https://github.com/Harmaniacz');
     console.log("The Harmaniacz were here!")
 }
 
 function home() {
-    window.open('home.html','_blank');
+    window.open('home.html');
 }
 
 function index() {
-    window.open('index.html', '_blank');
+    window.open('index.html');
 }
 
 function features() {
-    window.open('features.html', '_blank');
+    window.open('features.html');
 }
 
 function pricing() {
-    window.open('pricing.html', '_blank');
+    window.open('pricing.html');
 }
 
 function download() {
-    window.open('download.html', '_blank');
+    window.open('download.html');
 }
 
 function team() {
-    window.open('team.html', '_blank');
+    window.open('team.html');
 }
