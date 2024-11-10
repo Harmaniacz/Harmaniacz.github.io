1493661a79ef0167151230e052113e846fb6765f
yassmine <senyass02@gmail.com>
2024-11-09 22:56:17 -0500

    made the slider work - still need to work on buttons

---
+++ features.js
@@ -0,0 +1,14 @@
+new Swiper('.features-wrapper', {
+    loop: true,
+  
+    // Pagination bullets
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
